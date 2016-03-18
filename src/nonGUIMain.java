6514db667bc9dba07704848c6f31bdceeddcb4f7
Shady Atef <shadyoatef@gmail.com>
2016-03-18 18:28:56 +0200

    Initial Project Structure

---
+++ src/nonGUIMain.java
@@ -0,0 +1,12 @@
+/**
+ * Created by Shady Atef on 3/18/16.
+ * Copyrights Shadyoatef@gmail.com
+ *
+ */
+public class nonGUIMain {
+
+    public static void main(String[] args){
+
+        System.out.println("We are good enough");
+    }
+}
