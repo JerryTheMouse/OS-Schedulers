f423a042bffe0c767903c886546c2669fa262be1
Shady Atef <shadyoatef@gmail.com>
2016-03-27 02:25:23 +0200

    Added Few GUI tweaks
    1. Canvas now shows averge waiting time and the scheudler description
    2. Schedule button is only enabled if the processes table is not empty
    3.  Pressing Delete while highlighting a process will remove it from the table

--- src/gui/Controller.java
+++ src/gui/Controller.java
@@ -3,17 +3,20 @@ package gui;
 import data.Process;
 import data.WorkUnit;
 import javafx.collections.FXCollections;
+import javafx.collections.ListChangeListener;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.fxml.Initializable;
 import javafx.scene.canvas.Canvas;
 import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.control.*;
 import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.input.KeyCode;
+import javafx.scene.input.KeyEvent;
 import javafx.scene.paint.Color;
-import schedulers.AbstractScheduler;
-import schedulers.FCFS;
-import schedulers.RoundRobin;
+import javafx.scene.text.Text;
+import schedulers.*;
 
 import java.net.URL;
 import java.util.ArrayList;
@@ -40,11 +43,14 @@ public class Controller implements Initializable {
     public RadioButton RoundRobinRadio;
     public RadioButton FCFSRadio;
     public RadioButton SJF_PRadio;
+    public RadioButton SJF_NPRadio;
+
     // Canvas
     public Canvas GanttChartCanvas;
+    public Button scheduleBtn;
     private ArrayList<Process> processes = new ArrayList<>();
     private ObservableList<Process> observableProcesses = FXCollections.observableArrayList(processes);
-
+    
     public void Schedule(ActionEvent e) {
         AbstractScheduler scheduler = null;
         Toggle toggle = SchedulersToggleGroup.getSelectedToggle();
@@ -67,17 +73,21 @@ public class Controller implements Initializable {
             }
         });
 
-        drawGanttChart(scheduler.apply(processes));
+        drawGanttChart(scheduler, scheduler.apply(processes));
+
     }
 
-    private void drawGanttChart(LinkedList<WorkUnit> output) {
 
+    private void drawGanttChart(AbstractScheduler scheduler, LinkedList<WorkUnit> output) {
+
+        double requiredCanvasHeight;
         GraphicsContext gc = GanttChartCanvas.getGraphicsContext2D();
         gc.setFill(Color.BLUE);
         //Clear the previous chart
         gc.clearRect(0, 0, GanttChartCanvas.getWidth(), GanttChartCanvas.getHeight());
         double x = 10, y = 10, w = 40, h = 20;
-
+        requiredCanvasHeight = y + output.size() * h + 50;
+        GanttChartCanvas.setHeight(requiredCanvasHeight);
         GanttChartCanvas.setWidth(output.size() * w + 50);
 
         int counter = 0;
@@ -102,6 +112,51 @@ public class Controller implements Initializable {
             gc.fillText(String.valueOf(counter), x, y + 1.75 * h);
 
         }
+
+        // Draw a table which has the average waiting time
+        double nusOfProcesses = processes.size();
+        // the parameters for the table
+        x = 10;
+        w = 90;
+        y = y + h * 2.0;
+
+        // Check for the size of the canvas
+        if (GanttChartCanvas.getWidth() < 3 * w)
+            GanttChartCanvas.setWidth(3 * w);
+        requiredCanvasHeight += nusOfProcesses * h + h * 2;
+        GanttChartCanvas.setHeight(requiredCanvasHeight);
+
+        //The process name
+        gc.strokeRect(x, y, w, h);
+        gc.fillText("Process Id", x + 5, y + 0.75 * h);
+
+        // The process average time
+        gc.strokeRect(x + w, y, w, h);
+        gc.fillText("Avg waiting", x + w + 5, y + 0.75 * h);
+        y += h;
+        for (Process p : processes) {
+            //The process name
+            gc.strokeRect(x, y, w, h);
+            gc.fillText(String.format("P %d", p.getId()), x + 5, y + 0.75 * h);
+
+            // The process averge time
+            gc.strokeRect(x + w, y, w, h);
+            gc.fillText(String.format("%.2f", p.getWaitingTime() / nusOfProcesses), x + w + 5, y + 0.75 * h);
+
+            y += h;
+        }
+
+        Text SchedulerInfo = new Text(String.format("The Scheduler: %s \n Description : %s", scheduler.getName(), scheduler.getDescription()));
+        if (GanttChartCanvas.getWidth() < SchedulerInfo.getLayoutBounds().getWidth())
+            GanttChartCanvas.setWidth(SchedulerInfo.getLayoutBounds().getWidth() + 10);
+
+
+        requiredCanvasHeight += SchedulerInfo.getLayoutY() + 10;
+        GanttChartCanvas.setHeight(requiredCanvasHeight);
+
+        gc.fillText(SchedulerInfo.getText(), x, y + h);
+
+
     }
 
     public void AddProcess(ActionEvent e) {
@@ -140,5 +195,20 @@ public class Controller implements Initializable {
 
         //Initially selected round robin
         RoundRobinRadio.setSelected(true);
+
+        scheduleBtn.setDisable(true);
+        observableProcesses.addListener((ListChangeListener<Process>) c -> scheduleBtn.setDisable(c.getList().isEmpty()));
+
+
+        // Make pressing Del on tableview row to delete it
+        processesTable.setOnKeyPressed(new EventHandler<KeyEvent>() {
+            @Override
+            public void handle(KeyEvent event) {
+                if (event.getCode() == KeyCode.DELETE) {
+                    Process p = processesTable.getSelectionModel().getSelectedItem();
+                    observableProcesses.remove(p);
+                }
+            }
+        });
     }
 }
